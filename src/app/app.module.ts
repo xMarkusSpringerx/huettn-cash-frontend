3fffa05663d2e9f96ed98308b66b403d18b7bfb2
Markus Springer <markus.springer1994@googlemail.com>
2017-09-15 23:11:24 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { AddComponent } from './drinks/add/add.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrinksComponent,
+    AddComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule
